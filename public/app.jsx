a5601572dbaf770546e88bc7f7ac9b59e0dfa78b
Kostas Tsakas <kostastsakas@gmail.com>
2019-02-08 19:05:56 -0800

    add basic html

---
+++ public/app.jsx
@@ -0,0 +1,14 @@
+
+ReactDOM.render(
+    <div>
+        <h1>Meta Project</h1> 
+        <ul id="menu">
+            <li>Demo App</li>
+            <li>DevOps</li>
+            <li>Metrics</li>
+            <li>Docs & Guides</li>
+            <li>More</li>
+        </ul>
+    </div>,
+    document.getElementById('root')
+);
